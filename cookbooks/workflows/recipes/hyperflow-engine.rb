d4f9774dd83da501d874ec9bb41b180f6cae6530
Maciej Malawski <malawski@agh.edu.pl>
2015-02-12 11:46:47 +0100

    fixed nodejs installation

--- cookbooks/workflows/recipes/hyperflow-engine.rb
+++ cookbooks/workflows/recipes/hyperflow-engine.rb
@@ -3,33 +3,13 @@ package 'unzip'
 package 'build-essential' 
 
 
-include_recipe "nodejs"
-include_recipe "nodejs::npm"
+#include_recipe "nodejs"
+#include_recipe "nodejs::npm"
 
  
-version = 'master'
+version = 'develop'
 tarball = "#{version}.tar.gz"
-hyperflow_dir="/usr/local/hyperflow"
 
-remote_file "/tmp/#{tarball}" do
- source "https://github.com/dice-cyfronet/hyperflow/archive/#{tarball}"
- action :create_if_missing 
-end
-
-directory hyperflow_dir do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-end
-
-execute "unzip" do
- command "tar zxv --no-same-owner --no-same-permissions -f /tmp/#{tarball} --strip-components 1 -C #{hyperflow_dir}"
- creates hyperflow_dir + '/package.json'
- action :run
-end
-
-execute "install npm dependencies" do
- cwd hyperflow_dir
- command "npm install -d"
+execute "install hyperflow" do
+ command "npm install https://github.com/dice-cyfronet/hyperflow/archive/#{tarball}"
 end
