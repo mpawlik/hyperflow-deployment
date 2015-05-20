95823de538180ed9cf4bb7e70b45f218b59bc416
Maciej Malawski <malawski@agh.edu.pl>
2015-05-20 16:39:23 +0200

    Added attribute to enable services

--- cookbooks/workflows/recipes/redis.rb
+++ cookbooks/workflows/recipes/redis.rb
@@ -3,5 +3,7 @@ package 'redis-server'
 service "redis-server" do
   if node['paasage_services']['enable'] == false
     action [ :stop, :disable ]
+  else
+    action [ :start, :enable ]
   end
 end
