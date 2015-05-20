de859cab581bf07b56987475fdcfebdf8409a1be
Maciej Malawski <malawski@agh.edu.pl>
2015-05-20 16:21:52 +0200

    Added attribute to enable services

--- cookbooks/workflows/attributes/default.rb
+++ cookbooks/workflows/attributes/default.rb
@@ -3,4 +3,6 @@ include_attribute 'nodejs'
 default['amqp_executor']['AMQP_URL']      = "amqp://localhost:5672"
 default['amqp_executor']['AWS_ACCESS_KEY_ID'] = "defaultXXXXXXXXXXX"
 default['amqp_executor']['AWS_SECRET_ACCESS_KEY']    = "defaultYYYYYYYYYYYYYYYYYYY"
+default['paasage_services']['enable']      = true
+
 
