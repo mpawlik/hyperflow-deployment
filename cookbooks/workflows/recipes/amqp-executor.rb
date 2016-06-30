b6d575cc232995ecccf31f2b1a4542c497c859be
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-06-30 17:52:23 +0200

    removed paasage service

--- cookbooks/workflows/recipes/amqp-executor.rb
+++ cookbooks/workflows/recipes/amqp-executor.rb
@@ -1,6 +1,6 @@
-package 'build-essential' 
-package 'ruby'
-package 'ruby-dev'
+package 'build-essential'
+#package 'ruby'
+#package 'ruby-dev'
 package 'libxml2-dev'
 package 'libxslt-dev'
 package 'zlib1g-dev'
@@ -16,24 +16,4 @@ template "/etc/hyperflow-amqp-executor.yml" do
     owner "root"
     group "root"
     mode "0644"
-end
-
-service "hyperflow-amqp-executor" do
-  supports :restart => true, :start => true, :stop => true
-  if node['paasage_services']['enable'] == false
-    action [ :stop, :disable ]
-  else
-    action [ :start, :enable ]
-  end
-end
-
-
-template "hyperflow-amqp-executor" do
-  path "/etc/init.d/hyperflow-amqp-executor"
-  source "hyperflow-amqp-executor.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-  notifies :enable, "service[hyperflow-amqp-executor]"
-  notifies :start, "service[hyperflow-amqp-executor]"
 end
