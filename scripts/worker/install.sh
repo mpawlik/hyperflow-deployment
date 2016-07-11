e69cd5936ad2f0a69939579c8d0849ae87e2d422
Dennis Hoppe <hopped@users.noreply.github.com>
2016-07-11 14:05:48 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -29,7 +29,7 @@ do
     export AWS_SECRET_ACCESS_KEY=$3
     if [ -f ${FILE} ]; then
       echo "${FILE}: Using CLOUD storage for storing simulation results"
-      cat ${FILE} | jq --arg z ${AWS_REGION} --arg x ${AWS_ACCESS_KEY_ID} --arg y ${AWS_SECRET_ACCESS_KEY} '.amqp_executor=(.amqp_executor + { STORAGE: "cloud", REGION: $z, AWS_ACCESS_KEY_ID: $x, AWS_SECRET_ACCESS_KEY: $y })' > tmp.json
+      cat ${FILE} | jq --arg z ${AWS_REGION} --arg x ${AWS_ACCESS_KEY_ID} --arg y ${AWS_SECRET_ACCESS_KEY} '.amqp_executor=(.amqp_executor + { STORAGE: "cloud", AWS_REGION: $z, AWS_ACCESS_KEY_ID: $x, AWS_SECRET_ACCESS_KEY: $y })' > tmp.json
       mv tmp.json ${FILE}
     fi
   fi
