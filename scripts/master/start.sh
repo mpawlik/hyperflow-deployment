c8d6c7d99f5095c1cbbf9fc85f1271e9dab62e42
Dennis Hoppe <hopped@users.noreply.github.com>
2016-07-11 16:45:52 +0200

    Update start.sh

--- scripts/master/start.sh
+++ scripts/master/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 WORKDIR=$1
-S3_BUCKET=$2
-S3_PATH=$3
+S3_REGION=$2
+S3_BUCKET=$3
+S3_PATH=$4
 
 # Start RabbitMQ service
 service rabbitmq-server start
@@ -18,4 +19,7 @@ sleep 3
 nohup env PORT=51404 WORKDIR=${WORKDIR} S3_BUCKET=${S3_BUCKET} S3_PATH=${S3_PATH} AMQP_URL=$(cat /hyperflow-deployment/AMQP_URL) REDIS_URL=$(cat /hyperflow-deployment/REDIS_URL) /node_modules/hyperflow/bin/hflow start-server &
 
 # Run UI on port 80
+export AWS_S3_REGION=${S3_REGION}
+export AWS_S3_BUCKET=${S3_BUCKET}
+export AWS_S3_PATH=${S3_PATH}
 bash /hyperflow-ui/run.sh
